ccbeeefd7dc69ee73ed4d6664c2b4c25f80168d2
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2022-06-06 19:53:27 -0300

    Requisito 5 feito

---
+++ desafio5.sql
@@ -0,0 +1,17 @@
+SELECT
+	cancoes.musics AS cancao,
+    COUNT(users.usuario) AS reproducoes
+FROM
+	SpotifyClone.reproduz_uni AS reproduz_uni
+INNER JOIN
+	SpotifyClone.users AS users
+ON 
+	reproduz_uni.usuario_id = users.usuario_id
+INNER JOIN
+	SpotifyClone.cancoes AS cancoes
+ON 
+	cancoes.cancoes_id = reproduz_uni.cancoes_id
+GROUP BY
+	cancoes.musics
+ORDER BY
+	COUNT(users.usuario) DESC;
